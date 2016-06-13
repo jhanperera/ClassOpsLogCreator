89ef8bba49738ede287aa92907ace965f02a9360
Jhan Perera <jhanperera@live.com>
2016-06-13 13:35:41 -0400

    initialize layout and start work on 'Step 1'

--- ClassOpsLogCreator/ClassOpsLogCreator/Form1.cs
+++ ClassOpsLogCreator/ClassOpsLogCreator/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ClassOpsLogCreator
 {
-    public partial class Form1 : Form
+    public partial class LogCreator : Form
     {
-        public Form1()
+        public LogCreator()
         {
             InitializeComponent();
         }
--- ClassOpsLogCreator/ClassOpsLogCreator/Program.cs
+++ ClassOpsLogCreator/ClassOpsLogCreator/Program.cs
@@ -16,7 +16,7 @@ namespace ClassOpsLogCreator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LogCreator());
         }
     }
 }
