b8d758050a9998086ba809cf98f4d2b14d6ca60b
Jhan Perera <jhanperera@live.com>
2016-06-16 18:57:41 -0400

    Overhaul

--- ClassOpsLogCreator/ClassOpsLogCreator/Form1.Designer.cs
+++ ClassOpsLogCreator/ClassOpsLogCreator/Form1.Designer.cs
@@ -28,219 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.mainTabContainer = new System.Windows.Forms.TabControl();
-            this.step1Tab = new System.Windows.Forms.TabPage();
-            this.openBTN3 = new System.Windows.Forms.Button();
-            this.openBTN2 = new System.Windows.Forms.Button();
-            this.openBTN1 = new System.Windows.Forms.Button();
-            this.fileText3 = new System.Windows.Forms.Label();
-            this.fileText2 = new System.Windows.Forms.Label();
-            this.fileText1 = new System.Windows.Forms.Label();
-            this.fileTextBox3 = new System.Windows.Forms.TextBox();
-            this.fileTextBox2 = new System.Windows.Forms.TextBox();
-            this.fileTextBox1 = new System.Windows.Forms.TextBox();
-            this.setOneText = new System.Windows.Forms.Label();
-            this.clearBTN = new System.Windows.Forms.Button();
-            this.submitBTN = new System.Windows.Forms.Button();
-            this.step2Tab = new System.Windows.Forms.TabPage();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
-            this.mainTabContainer.SuspendLayout();
-            this.step1Tab.SuspendLayout();
+            this.createBTN = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // mainTabContainer
-            // 
-            this.mainTabContainer.Controls.Add(this.step1Tab);
-            this.mainTabContainer.Controls.Add(this.step2Tab);
-            this.mainTabContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mainTabContainer.Location = new System.Drawing.Point(0, 0);
-            this.mainTabContainer.Name = "mainTabContainer";
-            this.mainTabContainer.SelectedIndex = 0;
-            this.mainTabContainer.Size = new System.Drawing.Size(659, 257);
-            this.mainTabContainer.TabIndex = 0;
-            // 
-            // step1Tab
-            // 
-            this.step1Tab.Controls.Add(this.openBTN3);
-            this.step1Tab.Controls.Add(this.openBTN2);
-            this.step1Tab.Controls.Add(this.openBTN1);
-            this.step1Tab.Controls.Add(this.fileText3);
-            this.step1Tab.Controls.Add(this.fileText2);
-            this.step1Tab.Controls.Add(this.fileText1);
-            this.step1Tab.Controls.Add(this.fileTextBox3);
-            this.step1Tab.Controls.Add(this.fileTextBox2);
-            this.step1Tab.Controls.Add(this.fileTextBox1);
-            this.step1Tab.Controls.Add(this.setOneText);
-            this.step1Tab.Controls.Add(this.clearBTN);
-            this.step1Tab.Controls.Add(this.submitBTN);
-            this.step1Tab.Location = new System.Drawing.Point(4, 22);
-            this.step1Tab.Name = "step1Tab";
-            this.step1Tab.Padding = new System.Windows.Forms.Padding(3);
-            this.step1Tab.Size = new System.Drawing.Size(651, 231);
-            this.step1Tab.TabIndex = 0;
-            this.step1Tab.Text = "Step 1";
-            this.step1Tab.UseVisualStyleBackColor = true;
-            // 
-            // openBTN3
-            // 
-            this.openBTN3.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.openBTN3.Location = new System.Drawing.Point(496, 113);
-            this.openBTN3.Name = "openBTN3";
-            this.openBTN3.Size = new System.Drawing.Size(147, 20);
-            this.openBTN3.TabIndex = 11;
-            this.openBTN3.Text = "Open";
-            this.openBTN3.UseVisualStyleBackColor = true;
-            this.openBTN3.Click += new System.EventHandler(this.openBTN3_Click);
-            // 
-            // openBTN2
-            // 
-            this.openBTN2.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.openBTN2.Location = new System.Drawing.Point(496, 87);
-            this.openBTN2.Name = "openBTN2";
-            this.openBTN2.Size = new System.Drawing.Size(147, 20);
-            this.openBTN2.TabIndex = 10;
-            this.openBTN2.Text = "Open";
-            this.openBTN2.UseVisualStyleBackColor = true;
-            this.openBTN2.Click += new System.EventHandler(this.openBTN2_Click);
-            // 
-            // openBTN1
-            // 
-            this.openBTN1.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.openBTN1.Location = new System.Drawing.Point(496, 61);
-            this.openBTN1.Name = "openBTN1";
-            this.openBTN1.Size = new System.Drawing.Size(147, 20);
-            this.openBTN1.TabIndex = 9;
-            this.openBTN1.Text = "Open";
-            this.openBTN1.UseVisualStyleBackColor = true;
-            this.openBTN1.Click += new System.EventHandler(this.openBTN1_Click);
-            // 
-            // fileText3
-            // 
-            this.fileText3.AutoSize = true;
-            this.fileText3.Location = new System.Drawing.Point(34, 113);
-            this.fileText3.Name = "fileText3";
-            this.fileText3.Size = new System.Drawing.Size(53, 13);
-            this.fileText3.TabIndex = 8;
-            this.fileText3.Text = "File three:";
-            // 
-            // fileText2
-            // 
-            this.fileText2.AutoSize = true;
-            this.fileText2.Location = new System.Drawing.Point(34, 90);
-            this.fileText2.Name = "fileText2";
-            this.fileText2.Size = new System.Drawing.Size(46, 13);
-            this.fileText2.TabIndex = 7;
-            this.fileText2.Text = "File two:";
-            // 
-            // fileText1
-            // 
-            this.fileText1.AutoSize = true;
-            this.fileText1.Location = new System.Drawing.Point(34, 64);
-            this.fileText1.Name = "fileText1";
-            this.fileText1.Size = new System.Drawing.Size(47, 13);
-            this.fileText1.TabIndex = 6;
-            this.fileText1.Text = "File one:";
-            // 
-            // fileTextBox3
-            // 
-            this.fileTextBox3.Location = new System.Drawing.Point(87, 113);
-            this.fileTextBox3.Name = "fileTextBox3";
-            this.fileTextBox3.Size = new System.Drawing.Size(396, 20);
-            this.fileTextBox3.TabIndex = 5;
-            // 
-            // fileTextBox2
-            // 
-            this.fileTextBox2.Location = new System.Drawing.Point(87, 87);
-            this.fileTextBox2.Name = "fileTextBox2";
-            this.fileTextBox2.Size = new System.Drawing.Size(396, 20);
-            this.fileTextBox2.TabIndex = 4;
-            // 
-            // fileTextBox1
-            // 
-            this.fileTextBox1.Location = new System.Drawing.Point(87, 61);
-            this.fileTextBox1.Name = "fileTextBox1";
-            this.fileTextBox1.Size = new System.Drawing.Size(396, 20);
-            this.fileTextBox1.TabIndex = 3;
-            // 
-            // setOneText
-            // 
-            this.setOneText.AutoSize = true;
-            this.setOneText.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.setOneText.Location = new System.Drawing.Point(8, 15);
-            this.setOneText.Name = "setOneText";
-            this.setOneText.Size = new System.Drawing.Size(429, 29);
-            this.setOneText.TabIndex = 2;
-            this.setOneText.Text = "Please select the Excel Documents:";
-            // 
-            // clearBTN
-            // 
-            this.clearBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.clearBTN.Location = new System.Drawing.Point(37, 179);
-            this.clearBTN.Name = "clearBTN";
-            this.clearBTN.Size = new System.Drawing.Size(130, 35);
-            this.clearBTN.TabIndex = 1;
-            this.clearBTN.Text = "Clear";
-            this.clearBTN.UseVisualStyleBackColor = true;
-            this.clearBTN.Click += new System.EventHandler(this.clearBTN_Click);
-            // 
-            // submitBTN
-            // 
-            this.submitBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.submitBTN.AutoEllipsis = true;
-            this.submitBTN.Location = new System.Drawing.Point(496, 179);
-            this.submitBTN.Name = "submitBTN";
-            this.submitBTN.Size = new System.Drawing.Size(130, 35);
-            this.submitBTN.TabIndex = 0;
-            this.submitBTN.Text = "Submit";
-            this.submitBTN.UseVisualStyleBackColor = true;
-            // 
-            // step2Tab
-            // 
-            this.step2Tab.Location = new System.Drawing.Point(4, 22);
-            this.step2Tab.Name = "step2Tab";
-            this.step2Tab.Padding = new System.Windows.Forms.Padding(3);
-            this.step2Tab.Size = new System.Drawing.Size(651, 231);
-            this.step2Tab.TabIndex = 1;
-            this.step2Tab.Text = "Step 2";
-            this.step2Tab.UseVisualStyleBackColor = true;
             // 
             // openFileDialog
             // 
             this.openFileDialog.FileName = "openFileDialog";
             // 
+            // createBTN
+            // 
+            this.createBTN.Location = new System.Drawing.Point(77, 44);
+            this.createBTN.Name = "createBTN";
+            this.createBTN.Size = new System.Drawing.Size(176, 53);
+            this.createBTN.TabIndex = 0;
+            this.createBTN.Text = "Create";
+            this.createBTN.UseVisualStyleBackColor = true;
+            this.createBTN.Click += new System.EventHandler(this.createBTN_Click);
+            // 
             // LogCreator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(659, 257);
-            this.Controls.Add(this.mainTabContainer);
+            this.ClientSize = new System.Drawing.Size(339, 163);
+            this.Controls.Add(this.createBTN);
             this.Name = "LogCreator";
             this.Text = "Log Creator";
-            this.mainTabContainer.ResumeLayout(false);
-            this.step1Tab.ResumeLayout(false);
-            this.step1Tab.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TabControl mainTabContainer;
-        private System.Windows.Forms.TabPage step1Tab;
-        private System.Windows.Forms.TabPage step2Tab;
-        private System.Windows.Forms.Label setOneText;
-        private System.Windows.Forms.Button clearBTN;
-        private System.Windows.Forms.Button submitBTN;
-        private System.Windows.Forms.Button openBTN3;
-        private System.Windows.Forms.Button openBTN2;
-        private System.Windows.Forms.Button openBTN1;
-        private System.Windows.Forms.Label fileText3;
-        private System.Windows.Forms.Label fileText2;
-        private System.Windows.Forms.Label fileText1;
-        private System.Windows.Forms.TextBox fileTextBox3;
-        private System.Windows.Forms.TextBox fileTextBox2;
-        private System.Windows.Forms.TextBox fileTextBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button createBTN;
     }
 }
 
--- ClassOpsLogCreator/ClassOpsLogCreator/Form1.cs
+++ ClassOpsLogCreator/ClassOpsLogCreator/Form1.cs
@@ -13,73 +13,27 @@ namespace ClassOpsLogCreator
 {
     public partial class LogCreator : Form
     {
-        //Create our Excel objects
-        private static Excel.Application Excel1 = null;
-        private static Excel.Application Excel2 = null;
-        private static Excel.Application Excel3 = null;
+        // RoomSched
+        private static Excel.Application RoomSched = null;
 
-        //The work books for those excel objects
-        private static Excel.Workbook MyBook1 = null;
-        private static Excel.Workbook MyBook2 = null;
-        private static Excel.Workbook MyBook3 = null;
+        private static Excel.Workbook MyBookRoom = null;
 
-        //The sheets for those excel work books.
-        private static Excel.Worksheet MySheet1 = null;
-        private static Excel.Worksheet MySheet2 = null;
-        private static Excel.Worksheet MySheet3 = null;
+        private static Excel.Worksheet MySheetRoom = null;
 
         public LogCreator()
         {
             InitializeComponent();
         }
 
-        //Open a file dialog and set the text box with the file path
-        private void open_and_setFile(TextBox textBoxname)
+        private void createBTN_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Excel file|*.xlsx";
-            openFileDialog.Title = "Select a Excel File";
+            //Open the room excel file
+            RoomSched = new Excel.Application();
+            RoomSched.Visible = true;
+            MyBookRoom = RoomSched.Workbooks.Open("C:\\Users\\pereraj\\Desktop\\room schedule.xlsx");
+            MySheetRoom = MyBookRoom.Sheets[1];
 
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                textBoxname.Text = openFileDialog.FileName;
-            }
+            Excel.Range range1 = MySheetRoom.get_Range("A5", "A10");
         }
-
-        //call open_and_setFile() and create the excel components.
-        private void openBTN1_Click(object sender, EventArgs e)
-        {
-            open_and_setFile(fileTextBox1);
-            Excel1 = new Excel.Application();
-            Excel1.Visible = false;
-            MyBook1 = Excel1.Workbooks.Open(fileTextBox1.Text);
-            MySheet1 = MyBook1.Sheets[1];
-            
-        }
-      
-        private void openBTN2_Click(object sender, EventArgs e)
-        {
-            open_and_setFile(fileTextBox2);
-            Excel2 = new Excel.Application();
-            Excel2.Visible = false;
-            MyBook2 = Excel2.Workbooks.Open(fileTextBox2.Text);
-            MySheet2 = MyBook2.Sheets[1];
-        }
-
-        private void openBTN3_Click(object sender, EventArgs e)
-        {
-            open_and_setFile(fileTextBox3);
-            Excel3 = new Excel.Application();
-            Excel3.Visible = false;
-            MyBook3 = Excel3.Workbooks.Open(fileTextBox3.Text);
-            MySheet3 = MyBook3.Sheets[1];
-        }
-
-        private void clearBTN_Click(object sender, EventArgs e)
-        {
-            MyBook1.SaveAs("Text.pdf");
-            MyBook1.Close();
-        }
-
     }
 }
