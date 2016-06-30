547bae9e626d5862d2d4c5c7fa907d7479e4a1f3
Jhan Perera <jhanperera@live.com>
2016-06-30 13:04:23 -0400

    UI updates

--- ClassOpsLogCreator/ClassOpsLogCreator/Form1.Designer.cs
+++ ClassOpsLogCreator/ClassOpsLogCreator/Form1.Designer.cs
@@ -57,9 +57,10 @@
             // 
             // createBTN
             // 
-            this.createBTN.Location = new System.Drawing.Point(12, 273);
+            this.createBTN.Location = new System.Drawing.Point(18, 336);
+            this.createBTN.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.createBTN.Name = "createBTN";
-            this.createBTN.Size = new System.Drawing.Size(176, 37);
+            this.createBTN.Size = new System.Drawing.Size(264, 46);
             this.createBTN.TabIndex = 0;
             this.createBTN.Text = "Create Master Logout Log";
             this.createBTN.UseVisualStyleBackColor = true;
@@ -67,35 +68,39 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(194, 290);
+            this.textBox1.Location = new System.Drawing.Point(291, 357);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(323, 20);
+            this.textBox1.Size = new System.Drawing.Size(482, 22);
             this.textBox1.TabIndex = 1;
             // 
             // destinationLabel
             // 
             this.destinationLabel.AutoEllipsis = true;
             this.destinationLabel.AutoSize = true;
-            this.destinationLabel.Location = new System.Drawing.Point(194, 273);
+            this.destinationLabel.Location = new System.Drawing.Point(291, 336);
+            this.destinationLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.destinationLabel.Name = "destinationLabel";
-            this.destinationLabel.Size = new System.Drawing.Size(103, 13);
+            this.destinationLabel.Size = new System.Drawing.Size(148, 16);
             this.destinationLabel.TabIndex = 2;
             this.destinationLabel.Text = "Master Log location:";
             // 
             // startHour1
             // 
             this.startHour1.FormattingEnabled = true;
-            this.startHour1.Location = new System.Drawing.Point(7, 36);
+            this.startHour1.Location = new System.Drawing.Point(10, 44);
+            this.startHour1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.startHour1.Name = "startHour1";
-            this.startHour1.Size = new System.Drawing.Size(58, 21);
+            this.startHour1.Size = new System.Drawing.Size(85, 24);
             this.startHour1.TabIndex = 3;
             // 
             // am_pmCombo1
             // 
             this.am_pmCombo1.FormattingEnabled = true;
-            this.am_pmCombo1.Location = new System.Drawing.Point(72, 35);
+            this.am_pmCombo1.Location = new System.Drawing.Point(108, 43);
+            this.am_pmCombo1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.am_pmCombo1.Name = "am_pmCombo1";
-            this.am_pmCombo1.Size = new System.Drawing.Size(59, 21);
+            this.am_pmCombo1.Size = new System.Drawing.Size(86, 24);
             this.am_pmCombo1.TabIndex = 5;
             // 
             // mainGroupBox
@@ -117,9 +122,11 @@
             this.mainGroupBox.Controls.Add(this.numberOfShiftsCombo1);
             this.mainGroupBox.Controls.Add(this.startHour1);
             this.mainGroupBox.Controls.Add(this.am_pmCombo1);
-            this.mainGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.mainGroupBox.Location = new System.Drawing.Point(18, 15);
+            this.mainGroupBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.mainGroupBox.Name = "mainGroupBox";
-            this.mainGroupBox.Size = new System.Drawing.Size(505, 255);
+            this.mainGroupBox.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.mainGroupBox.Size = new System.Drawing.Size(758, 314);
             this.mainGroupBox.TabIndex = 6;
             this.mainGroupBox.TabStop = false;
             this.mainGroupBox.Text = "Fill out this information:";
@@ -127,134 +134,150 @@
             // toLabel2
             // 
             this.toLabel2.AutoSize = true;
-            this.toLabel2.Location = new System.Drawing.Point(137, 142);
+            this.toLabel2.Location = new System.Drawing.Point(206, 175);
+            this.toLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.toLabel2.Name = "toLabel2";
-            this.toLabel2.Size = new System.Drawing.Size(16, 13);
+            this.toLabel2.Size = new System.Drawing.Size(21, 16);
             this.toLabel2.TabIndex = 28;
             this.toLabel2.Text = "to";
             // 
             // endHour2
             // 
             this.endHour2.FormattingEnabled = true;
-            this.endHour2.Location = new System.Drawing.Point(159, 139);
+            this.endHour2.Location = new System.Drawing.Point(238, 171);
+            this.endHour2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.endHour2.Name = "endHour2";
-            this.endHour2.Size = new System.Drawing.Size(59, 21);
+            this.endHour2.Size = new System.Drawing.Size(86, 24);
             this.endHour2.TabIndex = 25;
             // 
             // am_pmCombo4
             // 
             this.am_pmCombo4.FormattingEnabled = true;
-            this.am_pmCombo4.Location = new System.Drawing.Point(224, 139);
+            this.am_pmCombo4.Location = new System.Drawing.Point(336, 171);
+            this.am_pmCombo4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.am_pmCombo4.Name = "am_pmCombo4";
-            this.am_pmCombo4.Size = new System.Drawing.Size(59, 21);
+            this.am_pmCombo4.Size = new System.Drawing.Size(86, 24);
             this.am_pmCombo4.TabIndex = 27;
             // 
             // numberOfShiftsLabel2
             // 
             this.numberOfShiftsLabel2.AutoSize = true;
-            this.numberOfShiftsLabel2.Location = new System.Drawing.Point(6, 169);
+            this.numberOfShiftsLabel2.Location = new System.Drawing.Point(9, 208);
+            this.numberOfShiftsLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.numberOfShiftsLabel2.Name = "numberOfShiftsLabel2";
-            this.numberOfShiftsLabel2.Size = new System.Drawing.Size(86, 13);
+            this.numberOfShiftsLabel2.Size = new System.Drawing.Size(123, 16);
             this.numberOfShiftsLabel2.TabIndex = 24;
             this.numberOfShiftsLabel2.Text = "Number of shifts:";
             // 
             // shiftTime2
             // 
             this.shiftTime2.AutoSize = true;
-            this.shiftTime2.Location = new System.Drawing.Point(6, 127);
+            this.shiftTime2.Location = new System.Drawing.Point(9, 156);
+            this.shiftTime2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.shiftTime2.Name = "shiftTime2";
-            this.shiftTime2.Size = new System.Drawing.Size(122, 13);
+            this.shiftTime2.Size = new System.Drawing.Size(180, 16);
             this.shiftTime2.TabIndex = 23;
             this.shiftTime2.Text = "Please select shift times:";
             // 
             // numberOfShiftsCombo2
             // 
             this.numberOfShiftsCombo2.FormattingEnabled = true;
-            this.numberOfShiftsCombo2.Location = new System.Drawing.Point(6, 185);
+            this.numberOfShiftsCombo2.Location = new System.Drawing.Point(9, 228);
+            this.numberOfShiftsCombo2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numberOfShiftsCombo2.Name = "numberOfShiftsCombo2";
-            this.numberOfShiftsCombo2.Size = new System.Drawing.Size(121, 21);
+            this.numberOfShiftsCombo2.Size = new System.Drawing.Size(180, 24);
             this.numberOfShiftsCombo2.TabIndex = 22;
             // 
             // startHour2
             // 
             this.startHour2.FormattingEnabled = true;
-            this.startHour2.Location = new System.Drawing.Point(6, 142);
+            this.startHour2.Location = new System.Drawing.Point(9, 175);
+            this.startHour2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.startHour2.Name = "startHour2";
-            this.startHour2.Size = new System.Drawing.Size(59, 21);
+            this.startHour2.Size = new System.Drawing.Size(86, 24);
             this.startHour2.TabIndex = 19;
             // 
             // am_pmCombo3
             // 
             this.am_pmCombo3.FormattingEnabled = true;
-            this.am_pmCombo3.Location = new System.Drawing.Point(72, 142);
+            this.am_pmCombo3.Location = new System.Drawing.Point(108, 175);
+            this.am_pmCombo3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.am_pmCombo3.Name = "am_pmCombo3";
-            this.am_pmCombo3.Size = new System.Drawing.Size(59, 21);
+            this.am_pmCombo3.Size = new System.Drawing.Size(86, 24);
             this.am_pmCombo3.TabIndex = 21;
             // 
             // toLabel1
             // 
             this.toLabel1.AutoSize = true;
-            this.toLabel1.Location = new System.Drawing.Point(137, 38);
+            this.toLabel1.Location = new System.Drawing.Point(206, 47);
+            this.toLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.toLabel1.Name = "toLabel1";
-            this.toLabel1.Size = new System.Drawing.Size(16, 13);
+            this.toLabel1.Size = new System.Drawing.Size(21, 16);
             this.toLabel1.TabIndex = 18;
             this.toLabel1.Text = "to";
             // 
             // endHour1
             // 
             this.endHour1.FormattingEnabled = true;
-            this.endHour1.Location = new System.Drawing.Point(159, 35);
+            this.endHour1.Location = new System.Drawing.Point(238, 43);
+            this.endHour1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.endHour1.Name = "endHour1";
-            this.endHour1.Size = new System.Drawing.Size(59, 21);
+            this.endHour1.Size = new System.Drawing.Size(86, 24);
             this.endHour1.TabIndex = 15;
             // 
             // am_pmCombo2
             // 
             this.am_pmCombo2.FormattingEnabled = true;
-            this.am_pmCombo2.Location = new System.Drawing.Point(224, 35);
+            this.am_pmCombo2.Location = new System.Drawing.Point(336, 43);
+            this.am_pmCombo2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.am_pmCombo2.Name = "am_pmCombo2";
-            this.am_pmCombo2.Size = new System.Drawing.Size(59, 21);
+            this.am_pmCombo2.Size = new System.Drawing.Size(86, 24);
             this.am_pmCombo2.TabIndex = 17;
             // 
             // numberOfShiftsLabel1
             // 
             this.numberOfShiftsLabel1.AutoSize = true;
-            this.numberOfShiftsLabel1.Location = new System.Drawing.Point(7, 62);
+            this.numberOfShiftsLabel1.Location = new System.Drawing.Point(10, 76);
+            this.numberOfShiftsLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.numberOfShiftsLabel1.Name = "numberOfShiftsLabel1";
-            this.numberOfShiftsLabel1.Size = new System.Drawing.Size(86, 13);
+            this.numberOfShiftsLabel1.Size = new System.Drawing.Size(123, 16);
             this.numberOfShiftsLabel1.TabIndex = 8;
             this.numberOfShiftsLabel1.Text = "Number of shifts:";
             // 
             // shiftTime1
             // 
             this.shiftTime1.AutoSize = true;
-            this.shiftTime1.Location = new System.Drawing.Point(7, 20);
+            this.shiftTime1.Location = new System.Drawing.Point(10, 25);
+            this.shiftTime1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.shiftTime1.Name = "shiftTime1";
-            this.shiftTime1.Size = new System.Drawing.Size(122, 13);
+            this.shiftTime1.Size = new System.Drawing.Size(180, 16);
             this.shiftTime1.TabIndex = 7;
             this.shiftTime1.Text = "Please select shift times:";
             // 
             // numberOfShiftsCombo1
             // 
             this.numberOfShiftsCombo1.FormattingEnabled = true;
-            this.numberOfShiftsCombo1.Location = new System.Drawing.Point(7, 78);
+            this.numberOfShiftsCombo1.Location = new System.Drawing.Point(10, 96);
+            this.numberOfShiftsCombo1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numberOfShiftsCombo1.Name = "numberOfShiftsCombo1";
-            this.numberOfShiftsCombo1.Size = new System.Drawing.Size(121, 21);
+            this.numberOfShiftsCombo1.Size = new System.Drawing.Size(180, 24);
             this.numberOfShiftsCombo1.TabIndex = 6;
             // 
             // workProgressBar
             // 
-            this.workProgressBar.Location = new System.Drawing.Point(12, 316);
+            this.workProgressBar.Location = new System.Drawing.Point(18, 389);
+            this.workProgressBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.workProgressBar.Name = "workProgressBar";
-            this.workProgressBar.Size = new System.Drawing.Size(505, 23);
+            this.workProgressBar.Size = new System.Drawing.Size(758, 28);
             this.workProgressBar.TabIndex = 7;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ClassOpsLogCreator.Properties.Resources.www_zaxonusa_com_small_icon_schedual;
-            this.pictureBox1.Location = new System.Drawing.Point(301, 14);
+            this.pictureBox1.Location = new System.Drawing.Point(452, 17);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(198, 235);
+            this.pictureBox1.Size = new System.Drawing.Size(297, 289);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
@@ -262,21 +285,26 @@
             // LogCreator
             // 
             this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Menu;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(524, 344);
+            this.ClientSize = new System.Drawing.Size(786, 423);
             this.Controls.Add(this.workProgressBar);
             this.Controls.Add(this.mainGroupBox);
             this.Controls.Add(this.destinationLabel);
             this.Controls.Add(this.createBTN);
             this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "LogCreator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Log Creator";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
             this.mainGroupBox.ResumeLayout(false);
             this.mainGroupBox.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- ClassOpsLogCreator/ClassOpsLogCreator/Properties/Resources.Designer.cs
+++ ClassOpsLogCreator/ClassOpsLogCreator/Properties/Resources.Designer.cs
@@ -63,26 +63,6 @@ namespace ClassOpsLogCreator.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap imageedit_3_6118778519 {
-            get {
-                object obj = ResourceManager.GetObject("imageedit_3_6118778519", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap schedule_512_682_icon {
-            get {
-                object obj = ResourceManager.GetObject("schedule_512_682_icon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap www_zaxonusa_com_small_icon_schedual {
             get {
                 object obj = ResourceManager.GetObject("www.zaxonusa.com-small-icon-schedual", resourceCulture);
