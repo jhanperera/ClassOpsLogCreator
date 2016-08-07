d6fa36bc2ae6e170d6a15e4558cf6723bb900e45
Jhan Perera <jhanperera@live.com>
2016-08-07 16:19:32 -0400

    Add the LogVIewer windows

---
+++ ClassOpsLogCreator/ClassOpsLogCreator/LogViewer.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ClassOpsLogCreator
+{
+    partial class LogViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.logCreatorBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timeLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logCreatorBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 97);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(897, 546);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // logCreatorBindingSource
+            // 
+            this.logCreatorBindingSource.DataSource = typeof(ClassOpsLogCreator.LogCreator);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(380, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 44);
+            this.textBox1.TabIndex = 1;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(546, 53);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(86, 31);
+            this.timeLabel.TabIndex = 2;
+            this.timeLabel.Text = "label1";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 655);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logCreatorBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource logCreatorBindingSource;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
---
+++ ClassOpsLogCreator/ClassOpsLogCreator/LogViewer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ClassOpsLogCreator
+{
+    public partial class LogViewer : Form
+    {
+        public readonly string EXISTING_MASTER_LOG = @"C:\Users\jhan\Documents\Visual Studio 2015\Projects\ClassOpsLogCreator\CLASSOPS\masterlog.xlsx";
+
+        public LogViewer()
+        {
+            InitializeComponent();
+
+            Excel.Application appExl;
+            Excel.Workbook workbook;
+            Excel.Worksheet NwSheet;
+            Excel.Range ShtRange;
+            appExl = new Excel.ApplicationClass();
+            workbook = appExl.Workbooks.Open((EXISTING_MASTER_LOG));
+            NwSheet = (Excel.Worksheet)workbook.Sheets.get_Item(1);
+
+            int Cnum = 0;
+            int Rnum = 0;
+
+            ShtRange = NwSheet.UsedRange;
+            DataTable dt = new DataTable();
+            dt.Columns.Add("name");
+            dt.Columns.Add("address");
+            // dt.Columns.Add("Status");
+            dt.Columns.Add("Phone1");
+            dt.Columns.Add("Phone2");
+            dt.Columns.Add("Phone3");
+            dt.Columns.Add("Phone4");
+            dt.Columns.Add("Phone5s");
+
+            Excel.Range last = NwSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+            Excel.Range shtRange = NwSheet.get_Range("B2", last);
+            System.Array classArray = (System.Array)shtRange.Cells.Value2;
+
+            for (Rnum = 1; Rnum <= classArray.GetUpperBound(0); Rnum++)
+            {
+                DataRow dr = dt.NewRow();
+                for (Cnum = 1; Cnum <= classArray.GetUpperBound(1); Cnum++)
+                {
+                    if(classArray.GetValue(Rnum, Cnum) == null)
+                    {
+                        dr[Cnum - 1] = "";
+                    }
+                    else
+                    {
+                        dr[Cnum - 1] = classArray.GetValue(Rnum, Cnum).ToString();
+                    }                
+                }
+                dt.Rows.Add(dr);
+                dt.AcceptChanges();
+            }
+            workbook.Close(true);
+            appExl.Quit();
+
+
+            //Session["data"] = dt; 
+            dataGridView1.DataSource = dt;
+            //dataGridView1.DataBind();*/
+        }
+    }
+}
