24d64832c23eddd32f3ef66e9860b02b8f01969d
Jhan Perera <jhanperera@live.com>
2016-08-10 13:43:52 -0400

    Added icon

--- ClassOpsLogCreator/ClassOpsLogCreator/Properties/AssemblyInfo.cs
+++ ClassOpsLogCreator/ClassOpsLogCreator/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassOpsLogCreator")]
+[assembly: AssemblyTitle("ClassOpsLogCreator - BETA")]
 [assembly: AssemblyDescription("This applicaion is for UIT Client Services to help with Classroom Operations")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jhan Perera")]
 [assembly: AssemblyProduct("ClassOpsLogCreator")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("JhanP")]
+[assembly: AssemblyTrademark("Jhan_P")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.0")]
-[assembly: AssemblyFileVersion("0.0.3.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
